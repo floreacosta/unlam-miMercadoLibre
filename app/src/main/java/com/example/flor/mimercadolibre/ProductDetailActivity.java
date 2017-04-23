8fbaca1e5caff6f37bd6df5dc64c361a2f91d462
floreacosta <florem.10@hotmail.com>
2017-04-23 13:47:42 -0300

    button onClick

--- app/src/main/java/com/example/flor/mimercadolibre/ProductDetailActivity.java
+++ app/src/main/java/com/example/flor/mimercadolibre/ProductDetailActivity.java
@@ -1,7 +1,10 @@
 package com.example.flor.mimercadolibre;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProductDetailActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class ProductDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detail);
+
+        Button buttonBuyPdp = (Button) findViewById(R.id.buttonBuyPdp);
+        buttonBuyPdp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setBackgroundColor(Color.parseColor("#2193af"));
+            }
+        });
     }
 }
